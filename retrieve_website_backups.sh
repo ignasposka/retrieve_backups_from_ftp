214cc0279bc0806fb4ea9c1f082373f1f1e06bb8
Ignas <Ignasp789@gmail.com>
2018-05-31 15:52:46 +0300

    Made path more abstract.

--- retrieve_website_backups.sh
+++ retrieve_website_backups.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -r -np -l1 -A "backup*mybakery.tar.gz" -P /home/buh/backups --user="root" --password="admin" ftp://ftp.mybakery.com
+wget -r -np -l1 -A "backup*mybakery.tar.gz" -P /your/path/to/backups --user="root" --password="admin" ftp://ftp.mybakery.com
 exit 0
